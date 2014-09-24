cd38cebb28cd7b323811a780d01ede1106ed22a6
NicolasVL <nvillegas809@gmail.com>
2014-09-23 19:42:32 -0500

    Final del desarrollo

--- P3_prac1/Program.cs
+++ P3_prac1/Program.cs
@@ -17,6 +17,10 @@ namespace P3_prac1
 			Console.WriteLine("Dame el primer numero");
 			int numero1 = int.Parse(Console.ReadLine());
 			Console.WriteLine("Dame el segundo numero");
-			int numero2 = int.Parse(Console.ReadLine());		}
+			int numero2 = int.Parse(Console.ReadLine());
+			
+			int resultado = numero1 - numero2;
+			Console.WriteLine("El resultado es: " + resultado);
+		}		
 	}
 }
